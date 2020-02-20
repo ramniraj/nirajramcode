76fbef5fb359e6aa8f4e70997b4334aea5838796
Niraj <ramniraj1987@gmail.com>
2020-02-20 23:25:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true ,useUnifiedTopology: true });
+
+
+const articleSchema = {
+
+    title : String,
+    content : String
+}; 
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req , res){
+
+    
+
+    Article.find (function( err , foundArticles){
+
+        if (!err){
+            res.send(foundArticles);
+        }else{
+            res.send(err);
+        }
+    });
+});
+
+app.post("/articles", function(req , res){
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    
+    });
+
+    newArticle.save(function(err){
+        if(!err){        
+            res.send("successfully addded  a new article");
+        }else {
+            res.send(err)
+        }
+    });
+    
+});
+
+app.listen(6001, function() {
+  console.log("Server started on port 6001");
+});
